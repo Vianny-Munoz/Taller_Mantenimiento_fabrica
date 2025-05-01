303fbe3aae6cb541620c1c2cfc05cd2caa558422
MadelinMunoz <titaniamade@gmail.com>
2025-04-30 23:21:43 -0500

    Insidencias corregido

--- agregar_incidencia.php
+++ agregar_incidencia.php
@@ -2,6 +2,21 @@
 
 include 'ConexionDB/conexion.php';
 
+//Validar Fechas
+$fechaIncidencia = $_POST['fecha_incidencia'];
+$fechaReparacion = $_POST['fecha_reparacion'];
+$hoy = date('Y-m-d');
+
+if($fechaIncidencia > $hoy){
+        header("location: incidencias.php?mensaje=La fecha ingreso no puede ser mayor a hoy");
+        exit();
+}
+
+if($fechaReparacion < $fechaIncidencia){
+        header("location: incidencias.php?mensaje=La fecha de reparacion no puede ser mayor a la de ingreso");
+        exit();
+}
+
 $codigo_inventario = $_POST['codigo_inventario'];
 $cedula_tecnico = $_POST['cedula_tecnico'];
 $fecha_incidencia = $_POST['fecha_incidencia'];
